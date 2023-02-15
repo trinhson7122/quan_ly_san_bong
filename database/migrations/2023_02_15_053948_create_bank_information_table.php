2a5cd13b89dc32bd0ba9eead260bdb8950c280ec
trinhson7122 <son07012002@gmail.com>
2023-02-15 20:31:38 +0700

    add migration

---
+++ database/migrations/2023_02_15_053948_create_bank_information_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bank_information', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('bank_numer');
+            $table->string('bank');
+            $table->string('note')->nullable();
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bank_information');
+    }
+};
